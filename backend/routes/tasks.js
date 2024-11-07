80b5396b528ad90d9e6888accbef6482372d6b6f
Zainal <shariff.zainal7@gmail.com>
2024-11-06 20:48:03 -0800

    Implemented input validation

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,20 +1,32 @@
 const express = require('express')
 const router = express.Router();
 const Task = require('../model/Task')
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
+const { check } = require('express-validator');
 
 router.use(auth);
 
 // Create a new task
-router.post('/', async(req, res) => {
-    try {
-        const task = new Task({...req.body, user: req.user});
-        await task.save();
-        res.status(201).json(task);
-    }
-    catch (error) {
-        res.status(400).json({message: error.message});
-    }
+router.post('/', 
+    [
+        check('title', 'Title is requried').not().isEmpty(),
+    ],
+    async(req, res) => {
+    
+        const errors = validationResult(req);
+    
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const task = new Task({...req.body, user: req.user});
+            await task.save();
+            res.status(201).json(task);
+        }
+        catch (error) {
+            res.status(400).json({message: error.message});
+        }
 });
 
 // Get all tasks
@@ -29,18 +41,27 @@ router.get('/', async (req, res) => {
 })
 
 // Update a task by ID
-router.put('/:id', async (req, res) => {
-    try {
-        const task = await Task.findByIdAndUpdate({_id: req.params.id, user: req.user}, req.body, {new: true});
-        if(!task){
-            return res.status(404).json({message: 'Task not found'});
+router.put('/:id', 
+    [
+        check('title', 'Title is required').optional().not().isEmpty(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
+        
+        try {
+            const task = await Task.findByIdAndUpdate({_id: req.params.id, user: req.user}, req.body, {new: true});
+            if(!task){
+                return res.status(404).json({message: 'Task not found'});
+            }
 
-        res.json(task);
-    }
-    catch (error){
-        res.status(400).json( {message: error.message});
-    }
+            res.json(task);
+        }
+        catch (error){
+            res.status(400).json( {message: error.message});
+        }
 })
 
 // Delete a task by ID
