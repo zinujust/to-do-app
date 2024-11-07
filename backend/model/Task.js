52f76c374245ee3a4f652cbe015a3af406fdfabd
Zainal <shariff.zainal7@gmail.com>
2024-11-06 20:19:00 -0800

    Added authentication to all routes

--- backend/model/Task.js
+++ backend/model/Task.js
@@ -4,6 +4,7 @@ const taskSchema = new mongoose.Schema({
     title: { type: String, required: true },
     completed: { type: Boolean, required: true },
     createdAt: { type: Date, default: Date.now },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
 });
 
 module.exports = mongoose.model('Task', taskSchema);
